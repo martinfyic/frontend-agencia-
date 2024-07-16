420dedd79e839065392e45160ea58c2594d9a6ff
martinfyic <martin.f.yic@gmail.com>
2024-07-16 20:00:36 -0300

    feat: :tada: start project - initial setup

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  author: 'Martín Ferreira',
+  description: 'Site Web de proyecto venta de excursiones, creado con Next.js',
+  keywords: ['excursion', 'viaje', 'paisaje', 'travel', 'descanso', 'placer'],
+  links: {
+    linkedin: 'https://www.linkedin.com/in/martin-ferreira-yic/',
+    github: 'https://github.com/martinfyic',
+    personalSite: 'https://www.martin-ferreira.com/',
+  },
+  name: 'Excursiones',
+};
+
+export type SiteConfig = typeof siteConfig;
