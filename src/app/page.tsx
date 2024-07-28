c356e0896a22f1cd7ae7efe4e62854fc0a15dfcf
martinfyic <martin.f.yic@gmail.com>
2024-07-28 17:59:01 -0300

    feat: :lipstick: agrego login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1 className='text-5xl'>Inicio de proyecto 🚀</h1>
+      <nav>
+        <Link
+          href='/auth/login'
+          className={cn(
+            'rounded-md bg-slate-700 px-2 py-2 text-slate-200 shadow-md',
+          )}
+        >
+          Login
+        </Link>
+      </nav>
     </main>
   );
 }
