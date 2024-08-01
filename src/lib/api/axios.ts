8b0ed47a5adf12d7ff06687a5b0e35ff8bb497ab
martinfyic <martin.f.yic@gmail.com>
2024-07-31 21:51:15 -0300

    refactor: :zap: add content-type application json at axios

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const apiBackend = axios.create({
   baseURL: 'http://localhost:3001/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default apiBackend;
